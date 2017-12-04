339943e4f8643f62b8b0c4c2fe532bc2fb9058e6
qwerty2501 <qwerty2501@live.jp>
2017-12-05 08:38:43 +0900

    added constructor.

--- src/main/scala/net/qwerty2501/radoc/APIDocumentWithVersion.scala
+++ src/main/scala/net/qwerty2501/radoc/APIDocumentWithVersion.scala
@@ -1,8 +1,9 @@
 package net.qwerty2501.radoc
 
-case class APIDocumentWithVersion(documents: Map[Version, APIDocument]) {}
+case class APIDocumentWithVersion(documents: Map[Version, APIDocument]) {
+  def this() = this(Map())
+}
 
 object APIDocumentWithVersion {
-  def apply(): APIDocumentWithVersion =
-    new APIDocumentWithVersion(Map())
+  def apply(): APIDocumentWithVersion = new APIDocumentWithVersion()
 }
