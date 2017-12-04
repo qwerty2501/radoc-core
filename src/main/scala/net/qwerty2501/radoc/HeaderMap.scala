ff88bcfb51f3ad08c15580f2babcd7024c5ab1b1
qwerty2501 <qwerty2501@live.jp>
2017-12-05 08:47:01 +0900

    mutable to immutable.

--- src/main/scala/net/qwerty2501/radoc/HeaderMap.scala
+++ src/main/scala/net/qwerty2501/radoc/HeaderMap.scala
@@ -1,12 +1,11 @@
 package net.qwerty2501.radoc
 
-import scala.collection.mutable
-trait HeaderMap extends mutable.Map[String, String]
+import scala.collection.immutable.HashMap
+
+trait HeaderMap extends Map[String, String]
 
 object HeaderMap {
   def apply(): HeaderMap = new HeaderMapImpl()
 }
 
-private class HeaderMapImpl
-    extends mutable.HashMap[String, String]
-    with HeaderMap {}
+private class HeaderMapImpl extends HashMap[String, String] with HeaderMap {}
