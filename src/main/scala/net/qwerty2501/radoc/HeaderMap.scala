cc382512113143f13402ca8f11195b27337a80e7
qwerty2501 <qwerty2501@live.jp>
2017-12-06 00:28:46 +0900

    specified immutable.

--- src/main/scala/net/qwerty2501/radoc/HeaderMap.scala
+++ src/main/scala/net/qwerty2501/radoc/HeaderMap.scala
@@ -1,6 +1,6 @@
 package net.qwerty2501.radoc
 
-import scala.collection.immutable.HashMap
+import scala.collection.immutable
 
 trait HeaderMap extends Map[String, String]
 
@@ -8,4 +8,6 @@ object HeaderMap {
   def apply(): HeaderMap = new HeaderMapImpl()
 }
 
-private class HeaderMapImpl extends HashMap[String, String] with HeaderMap {}
+private class HeaderMapImpl
+    extends immutable.HashMap[String, String]
+    with HeaderMap {}
