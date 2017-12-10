89709b256d923a2809de97d470ea6be5ebf2b9d6
qwerty2501 <qwerty2501@live.jp>
2017-12-10 15:23:49 +0900

    path type String to Path.

--- src/main/scala/net/qwerty2501/radoc/Request.scala
+++ src/main/scala/net/qwerty2501/radoc/Request.scala
@@ -1,54 +1,48 @@
 package net.qwerty2501.radoc
 
-import com.sun.net.httpserver.Headers
-
-import scala.tools.nsc.io.Path
 
 case class Request(method: Method,
-                   path: String,
+                   path: Path,
                    headers: HeaderMap,
                    content: Content) {
 
-  def this(method: Method, path: String, headers: HeaderMap) =
+  def this(method: Method, path: Path, headers: HeaderMap) =
     this(method, path, headers, NothingContent())
-  def this(method: Method, path: String, text: String) =
+  def this(method: Method, path: Path, text: String) =
     this(method, path, HeaderMap(), TextContent(text))
-  def this(method: Method, path: String) =
+  def this(method: Method, path: Path) =
     this(method, path, HeaderMap(), NothingContent())
-  def this(method: Method, path: String, content: Content) =
+  def this(method: Method, path: Path, content: Content) =
     this(method, path, HeaderMap(), content)
-  def this(method: Method, path: String, headers: HeaderMap, text: String) =
+  def this(method: Method, path: Path, headers: HeaderMap, text: String) =
     this(method, path, headers, TextContent(text))
 }
 
 object Request {
 
-  def get(path: String): Request = apply(Methods.GET, path)
-  def get(path: String, headers: HeaderMap) = apply(Methods.GET, path, headers)
-  def post(path: String, content: String) = apply(Methods.POST, path, content)
-  def post(path: String, headers: HeaderMap, content: String) =
+  def get(path: Path): Request = apply(Methods.GET, path)
+  def get(path: Path, headers: HeaderMap) = apply(Methods.GET, path, headers)
+  def post(path: Path, content: String) = apply(Methods.POST, path, content)
+  def post(path: Path, headers: HeaderMap, content: String) =
     apply(Methods.POST, path, headers, content)
-  def put(path: String, content: String) = apply(Methods.PUT, path, content)
-  def put(path: String, headers: HeaderMap, content: String) =
+  def put(path: Path, content: String) = apply(Methods.PUT, path, content)
+  def put(path: Path, headers: HeaderMap, content: String) =
     apply(Methods.PUT, path, headers, content)
 
-  def delete(path: String) = apply(Methods.DELETE, path)
-  def delete(path: String, headers: HeaderMap) =
+  def delete(path: Path) = apply(Methods.DELETE, path)
+  def delete(path: Path, headers: HeaderMap) =
     apply(Methods.DELETE, path, headers)
-  def delete(path: String, headers: HeaderMap, content: String) =
+  def delete(path: Path, headers: HeaderMap, content: String) =
     apply(Methods.DELETE, path, headers, content)
 
-  def apply(method: Method, path: String, headers: HeaderMap) =
+  def apply(method: Method, path: Path, headers: HeaderMap) =
     new Request(method, path, headers)
-  def apply(method: Method, path: String, text: String): Request =
+  def apply(method: Method, path: Path, text: String) =
     new Request(method, path, TextContent(text))
-  def apply(method: Method, path: String): Request =
+  def apply(method: Method, path: Path) =
     new Request(method, path)
-  def apply(method: Method, path: String, content: Content): Request =
+  def apply(method: Method, path: Path, content: Content) =
     new Request(method, path, content)
-  def apply(method: Method,
-            path: String,
-            headers: HeaderMap,
-            text: String): Request =
+  def apply(method: Method, path: Path, headers: HeaderMap, text: String) =
     new Request(method, path, headers, TextContent(text))
 }
