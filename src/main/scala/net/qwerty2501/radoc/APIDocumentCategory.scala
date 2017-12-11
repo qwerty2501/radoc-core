c9096373ec8c2c7a2db42b3d3754ae721ac7967a
qwerty2501 <qwerty2501@live.jp>
2017-12-12 00:11:07 +0900

    deleted unnecessary constructor.

--- src/main/scala/net/qwerty2501/radoc/APIDocumentCategory.scala
+++ src/main/scala/net/qwerty2501/radoc/APIDocumentCategory.scala
@@ -2,8 +2,4 @@ package net.qwerty2501.radoc
 
 private case class APIDocumentCategory(
     category: String,
-    requestResponseDocuments: Seq[APIDocumentGroup]) {
-
-  def this(requestResponseDocuments: Seq[APIDocumentGroup]) =
-    this("", requestResponseDocuments)
-}
+    apiDocumentGroups: Map[String, APIDocumentGroup]) {}
