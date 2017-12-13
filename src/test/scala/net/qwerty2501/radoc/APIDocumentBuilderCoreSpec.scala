a68ba32a570adcf16a1572daafef9d627b6cfcb5
qwerty2501 <qwerty2501@live.jp>
2017-12-14 01:24:56 +0900

    added test case

--- src/test/scala/net/qwerty2501/radoc/APIDocumentBuilderCoreSpec.scala
+++ src/test/scala/net/qwerty2501/radoc/APIDocumentBuilderCoreSpec.scala
@@ -1,10 +1,24 @@
 package net.qwerty2501.radoc
 
+import net.qwerty2501.radoc.mocks._
 import org.scalatest._
 
 class APIDocumentBuilderCoreSpec extends FlatSpec with Matchers {
 
   it should "can add request response" in {
+    val apiDocumentBuilder = new APIDocumentBuilderMock()
+    val path = "test/path"
+    val response = apiDocumentBuilder.request(Request.get(path))
 
+    val messageDocument = apiDocumentBuilder.getRootAPIDocument
+      .documents(Version())
+      .apiCategories("")
+      .apiDocumentGroups(path)
+      .apiDocuments(Method.Get, path)
+      .messageDocuments
+      .head
+
+    messageDocument.request.path.displayPath should be(path)
+    messageDocument.response should be(response)
   }
 }
