8b2faaaaba793eeda73e26b6b964263879c95df5
qwerty2501 <qwerty2501@live.jp>
2018-01-29 03:28:15 +0900

    added toString

--- src/main/scala/net/qwerty2501/radoc/APIDocumentGroup.scala
+++ src/main/scala/net/qwerty2501/radoc/APIDocumentGroup.scala
@@ -1,4 +1,9 @@
 package net.qwerty2501.radoc
 
 case class APIDocumentGroup(group: String,
-                            apiDocuments: Map[(Method, String), APIDocument])
+                            apiDocuments: Map[(Method, String), APIDocument],
+                            category: String,
+                            version: Version) {
+
+  override def toString: String = group
+}
