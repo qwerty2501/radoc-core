6ed619033a917430da2a5e681c9594c18b13d4d9
qwerty2501 <qwerty2501@live.jp>
2018-02-01 01:36:13 +0900

    added constructor and apply

--- src/main/scala/net/qwerty2501/radoc/Parameter.scala
+++ src/main/scala/net/qwerty2501/radoc/Parameter.scala
@@ -3,14 +3,34 @@ package net.qwerty2501.radoc
 case class Parameter(name: String,
                      value: Any,
                      typeName: String,
-                     description: Text)
+                     description: Text) {
+
+  def this(name: String,
+           value: Any,
+           typeNames: Seq[String],
+           description: Text) =
+    this(name, value, typeNames.mkString(" | "), description)
+
+  def this(name: String, value: Any, valueType: Class[_], description: Text) =
+    this(name, value, valueType.toString, description)
+
+  def this(name: String, value: Any, description: Text) =
+    this(name, value, value.getClass, description)
+
+}
 
 object Parameter {
   def apply(name: String, value: Any, description: Text): Parameter =
-    Parameter(name, value, value.getClass, description)
-  def apply[T](name: String,
-               value: Any,
-               valueType: Class[T],
-               description: Text): Parameter =
-    Parameter(name, value, valueType.getSimpleName, description)
+    new Parameter(name, value, value.getClass, description)
+  def apply(name: String,
+            value: Any,
+            valueType: Class[_],
+            description: Text): Parameter =
+    new Parameter(name, value, valueType, description)
+
+  def apply(name: String,
+            value: Any,
+            typeNames: Seq[String],
+            description: Text) =
+    new Parameter(name, value, typeNames, description)
 }
