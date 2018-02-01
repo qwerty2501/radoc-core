d7a773e70a010718baf6f624a340d603ade4ad0e
qwerty2501 <qwerty2501@live.jp>
2018-02-01 13:51:22 +0900

    added XML Content

--- src/main/scala/net/qwerty2501/radoc/Content.scala
+++ src/main/scala/net/qwerty2501/radoc/Content.scala
@@ -1,11 +1,22 @@
 package net.qwerty2501.radoc
 
+import scala.xml._
+
 trait Content {}
 
 object Content {
   def apply(): Content = NothingContent()
   def apply(text: String): Content =
     if (text != "") TextContent(text) else NothingContent()
+  def apply(element:Elem):Content = XMLContent(element)
+
+  def apply(text:String,contentType:String):Content = if( contentType.contains("json") ){
+    Content()
+  } else if (contentType.contains("xml")){
+    apply(xml.XML.loadString(text))
+  } else {
+    apply(text)
+  }
 }
 
 private case class NothingContent() extends Content {
@@ -13,5 +24,9 @@ private case class NothingContent() extends Content {
 }
 
 private case class TextContent(text: String) extends Content {
-  override def toString = text
+  override def toString:String = text
+}
+
+private case class XMLContent(element:Elem) extends Content{
+  override def toString: String =  Xhtml.toXhtml(element)
 }
