c63fe518f415efe2d32f744c745c4dd6f6825c44
qwerty2501 <qwerty2501@live.jp>
2018-02-06 22:52:05 +0900

    the description should be written.

--- radoc-core/src/main/java/net/qwerty2501/radoc/DocumentHint.java
+++ radoc-core/src/main/java/net/qwerty2501/radoc/DocumentHint.java
@@ -1,7 +1,7 @@
 package net.qwerty2501.radoc;
 
 public @interface DocumentHint {
-    String description() default "";
+    String description();
     String name() default "";
     boolean optional() default false;
     boolean xmlAttribute() default false;
