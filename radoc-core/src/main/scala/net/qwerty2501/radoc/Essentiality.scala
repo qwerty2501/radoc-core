aeaeb4d7fccd26df54d8748a173518bee28a21e8
qwerty2501 <qwerty2501@live.jp>
2018-02-10 03:58:32 +0900

    removed trash code.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/Essentiality.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/Essentiality.scala
@@ -2,8 +2,6 @@ package net.qwerty2501.radoc
 
 final class Essentiality private () {}
 
-final class Test3 {}
-
 object Essentiality {
   final val OmitEmpty: Essentiality = new Essentiality()
   final val Mandatory: Essentiality = new Essentiality()
