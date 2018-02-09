e533159e3312f77bbf33ef29430d4ef8282eb0cb
qwerty2501 <qwerty2501@live.jp>
2018-02-10 03:12:32 +0900

    removed default arguments.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/DocumentArgs.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/DocumentArgs.scala
@@ -1,15 +1,21 @@
 package net.qwerty2501.radoc
 
-case class DocumentArgs(category: String = "",
-                        group: String = "",
-                        description: Text = Text(),
-                        messageName: String = "",
-                        version: Version = Version.firstVersion) {
+case class DocumentArgs(category: String,
+                        group: String,
+                        description: Text,
+                        messageName: String,
+                        version: Version) {
+  def this() = this("", "", Text(), "", Version.empty)
+  def this(description: Text) =
+    this("", "", description, "", Version.empty)
   def this(category: String, description: Text, version: Version) =
     this(category, "", description, "", version)
 }
 
 object DocumentArgs {
+  val empty = new DocumentArgs()
+  def apply(): DocumentArgs = empty
+  def apply(description: Text): DocumentArgs = new DocumentArgs(description)
   def apply(category: String, description: Text, version: Version) =
     new DocumentArgs(category, description, version)
 }
--- radoc-core/src/main/scala/net/qwerty2501/radoc/Version.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/Version.scala
@@ -1,10 +1,11 @@
 package net.qwerty2501.radoc
 
-case class Version(major: Int, minor: Int, build: Int) {
+case class Version(prefix: String, major: Int, minor: Int, build: Int) {
   if (major < 0 || minor < 0 || build < 0) {
     throw new IllegalArgumentException()
   }
 
+  def this(major: Int, minor: Int, build: Int) = this("", major, minor, build)
   def this(major: Int, minor: Int) = this(major, minor, 0)
   def this(major: Int) = this(major, 0)
   def this() = this(0)
@@ -17,9 +18,11 @@ case class Version(major: Int, minor: Int, build: Int) {
 
 object Version {
   val firstVersion: Version = Version(0, 0, 1)
-  val empty: Version = Version()
-  def apply(): Version = Version(0)
-  def apply(major: Int): Version = Version(major, 0)
-  def apply(major: Int, minor: Int): Version = Version(major, minor, 0)
+  val empty: Version = new Version()
+  def apply(): Version = empty
+  def apply(major: Int): Version = new Version(major)
+  def apply(major: Int, minor: Int): Version = new Version(major, minor)
+  def apply(major: Int, minor: Int, build: Int): Version =
+    new Version(major, minor, build)
 
 }
