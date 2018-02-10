867b5afd63d9478cd4bc74ae7344ae0f81689705
qwerty2501 <qwerty2501@live.jp>
2018-02-10 20:29:23 +0900

    added Excluded.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/Essentiality.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/Essentiality.scala
@@ -5,4 +5,5 @@ final class Essentiality private () {}
 object Essentiality {
   final val OmitEmpty: Essentiality = new Essentiality()
   final val Mandatory: Essentiality = new Essentiality()
+  final val Excluded: Essentiality = new Essentiality()
 }
