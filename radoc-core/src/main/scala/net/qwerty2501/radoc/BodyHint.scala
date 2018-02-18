d612816416d8c29d8757bdced430e895f68a425f
qwerty2501 <qwerty2501@live.jp>
2018-02-18 23:01:07 +0900

    created ParameterBodyHint

--- radoc-core/src/main/scala/net/qwerty2501/radoc/BodyHint.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/BodyHint.scala
@@ -8,21 +8,34 @@ trait BodyHint {
   def upgrade(message: Message): BodyHint = {
     ContentType(message.headers) match {
       case ContentType.Json =>
-        new JsonBodyHintMerger().merge(
-          parse(message.body.contentText).getOrElse(Json.Null),
-          this match {
-            case jsonBodyHint: JsonBodyHint => jsonBodyHint
-            case _                          => JsonBodyHint()
-          })
+        this match {
+          case jsonBodyHint: JsonBodyHint =>
+            onJsonBodyHint(message, jsonBodyHint)
+          case parameterBodyHint: ParameterBodyHint => parameterBodyHint
+          case _                                    => onJsonBodyHint(message, JsonBodyHint())
+        }
 
       case _ => this
     }
   }
+
+  private def onJsonBodyHint(message: Message, jsonBodyHint: JsonBodyHint) =
+    new JsonBodyHintMerger().merge(
+      parse(message.body.contentText).getOrElse(Json.Null),
+      jsonBodyHint
+    )
 }
+
+private case class ParameterBodyHint(
+    typeParameterMap: Map[String, Seq[Parameter]])
+    extends BodyHint
 
 object BodyHint {
   val empty: BodyHint = new BodyHint {
     override val typeParameterMap: Map[String, Seq[Parameter]] = Map()
   }
   def apply(): BodyHint = empty
+
+  def apply(typeParameterMap: Map[String, Seq[Parameter]]): BodyHint =
+    ParameterBodyHint(typeParameterMap)
 }
