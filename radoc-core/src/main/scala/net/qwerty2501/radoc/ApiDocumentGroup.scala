0ab6c532aea120da00c40777e67707819e96e754
qwerty2501 <qwerty2501@live.jp>
2018-02-20 21:54:11 +0900

    deleted garbage code.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/ApiDocumentGroup.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/ApiDocumentGroup.scala
@@ -3,7 +3,4 @@ package net.qwerty2501.radoc
 case class ApiDocumentGroup(group: String,
                             apiDocuments: Map[(Method, String), ApiDocument],
                             category: String,
-                            version: Version) {
-
-  override def toString: String = group
-}
+                            version: Version) {}
